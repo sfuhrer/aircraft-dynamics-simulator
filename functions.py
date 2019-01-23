fixed issue with forces and moments calculation

functions.py
@@ -10,39 +10,35 @@ def aerodynamic_forces_moments(current_states, current_inputs):
     delta = [current_inputs[0], current_inputs[1]]
     rpm = [current_inputs[2], current_inputs[3]]
 
-    d_f_x_rpm2 = np.array([0.0, 0.0])
-    d_m_x_0 = np.array([0.0, 0.0])
-    d_m_x_delta = np.array([0.0, 0.0])
-    d_m_y_delta = np.array([0.0, 0.0])
-    f_x = f_y = f_z = m_x = m_y = m_z = [0.0, 0.0]
+    helper_f_x_rpm2 = np.array([1, V / (rpm_nom_cruise / 60.0)])
+    helper_m_x_0_0 = np.array([rpm[0] ** 2, V * rpm[0]])
+    helper_m_x_0_1 = np.array([rpm[1] ** 2, V * rpm[1]])
+    helper_m_x_y_delta_0 = np.array([V ** 2, rpm[0] ** 2, rpm[0] * V])
+    helper_m_x_y_delta_1 = np.array([V ** 2, rpm[1] ** 2, rpm[1] * V])
 
-    helper_f_x_rpm2 = np.array([1, V/(rpm_nom_cruise / 60)])
+    d_f_x_rpm2 = np.dot(coeff_f_x_rpm2, helper_f_x_rpm2)
+    d_m_x_0_0 = np.dot(coeff_m_x_0, helper_m_x_0_0)
+    d_m_x_0_1 = np.dot(coeff_m_x_0, helper_m_x_0_1)
+    d_m_x_delta_0 = np.dot(coeff_m_x_delta, helper_m_x_y_delta_0)
+    d_m_x_delta_1 = np.dot(coeff_m_x_delta, helper_m_x_y_delta_1)
+    d_m_y_delta_0 = np.dot(coeff_m_y_delta, helper_m_x_y_delta_0)
+    d_m_y_delta_1 = np.dot(coeff_m_y_delta, helper_m_x_y_delta_1)
 
-    # calculate separate forces and moments
-    for i in range(2):
-        helper_m_x_0 = np.array([rpm[i] * rpm[i], V*rpm[i]])
-        helper_m_x_y_delta = np.array([V*V, rpm[i] * rpm[i], rpm[i]*V])
+    f_x = [d_f_x_rpm2 * rpm[0] ** 2, d_f_x_rpm2 * rpm[1] ** 2]
+    m_x = [d_m_x_0_0 + d_m_x_delta_0*delta[0], d_m_x_0_1 + d_m_x_delta_1*delta[1]]
+    m_y = [d_m_y_delta_0 * delta[0], d_m_y_delta_1 * delta[1]]
 
-        d_f_x_rpm2[i] = np.dot(coeff_f_x_rpm2, helper_f_x_rpm2)
-        d_m_x_0[i] = np.dot(coeff_m_x_0, helper_m_x_0)
-        d_m_x_delta[i] = np.dot(coeff_m_x_delta, helper_m_x_y_delta)
-        d_m_y_delta[i] = np.dot(coeff_m_y_delta, helper_m_x_y_delta)
 
-        f_x[i] = d_f_x_rpm2[i] * rpm[i] * rpm[i]
-        m_x[i] = d_m_x_0 + d_m_x_delta * delta[i]
-        m_y[i] = d_m_y_delta * delta[i]
-
-        f_y[i] = 0
-        f_z[i] = 0
-        m_z[i] = 0
-
-    f_x = f_x[0] + f_x[1]
-    f_y = f_y[0] + f_y[1]
-    f_z = f_z[0] + f_z[1]
-    m_x = m_x[0] + m_x[1]
-    m_y = m_y[0] + m_y[1]
-    m_z = m_z[0] + m_z[1]
-    forces_moments = [f_x, f_y, f_z, m_x, m_y, m_z]
+    f_x_combined = f_x[0] + f_x[1]
+    # print(f_x[0])
+    # print(f_x_combined)
+    f_y_combined = 0.0
+    f_z_combined = 0.0
+    m_x_combined = m_x[0] + m_x[1]
+    m_y_combined = m_y[0] + m_y[1]
+    m_z_combined = 0.0
+    forces_moments = [f_x_combined, f_y_combined, f_z_combined, m_x_combined, m_y_combined, m_z_combined]
+    # print(forces_moments)
     return forces_moments
 
 
@@ -62,7 +58,8 @@ def calculate_state_differentials(F_M_aero, states):
     #  intermediate states
     u = V * math.cos(alpha) * math.cos(beta)
     v = V * math.sin(beta)
-    w: Union[float, Any] = V * math.sin(alpha) * math.cos(beta)
+    w = V * math.sin(alpha) * math.cos(beta)
+    # w: Union[float, Any] = V * math.sin(alpha) * math.cos(beta)
 
     # intermediate state differentials
     u_dot = r * v - q * w - g * math.sin(theta) + F_M_aero[0] / mass
@@ -71,14 +68,14 @@ def calculate_state_differentials(F_M_aero, states):
     
     # state differentials
     V_dot = (u * u_dot + v * v_dot + w * w_dot) / V  # airspeed
-    beta_dot = (V * v_dot - v * V_dot) / (V ** 2 * math.cos(beta))
-    alpha_dot = (u * w_dot - w * u_dot) / (u ** 2 + w ** 2)
+    beta_dot = (V * v_dot - v * V_dot) / (V**2 * math.cos(beta))
+    alpha_dot = (u * w_dot - w * u_dot) / (u**2 + w**2)
 
     I1 = Ixz * (Iyy - Ixx - Izz)
     I2 = (Ixx * Izz - Ixz ** 2)
-    p_dot = (Izz * F_M_aero[3] + Ixz * F_M_aero[5] - (I1 * p + (Ixz ** 2 + Izz * (Izz - Iyy)) * r) * q) / I2
-    q_dot = (F_M_aero[4] - (Ixx - Izz) * p * r - Ixz * (p ** 2 - r ** 2)) / Iyy  # neglecting thrusting moments
-    r_dot = (Ixz * F_M_aero[3] + Ixx * F_M_aero[5] + (I1 * r + (Ixz ** 2 + Ixx * (Ixx - Iyy)) * p) * q) / I2
+    p_dot = (Izz * F_M_aero[3] + Ixz * F_M_aero[5] - (I1 * p + (Ixz**2 + Izz * (Izz - Iyy)) * r) * q) / I2
+    q_dot = (F_M_aero[4] - (Ixx - Izz) * p * r - Ixz * (p**2 - r**2)) / Iyy  # neglecting thrusting moments
+    r_dot = (Ixz * F_M_aero[3] + Ixx * F_M_aero[5] + (I1 * r + (Ixz**2 + Ixx * (Ixx - Iyy)) * p) * q) / I2
 
     phi_dot = p + (q * math.sin(phi) + r * math.cos(phi)) * math.tan(theta)
     theta_dot = q * math.cos(phi) - r * math.sin(phi)
@@ -98,12 +95,13 @@ def calculate_state_differentials(F_M_aero, states):
 
     return state_differentials
 
+
 def plot_states_and_inputs(t_vec, x_array, u_array):
     # plotting
     # figure with longitudinal states and inputs
-    # plot velocity
 
-    plt.rcParams.update({'font.size': 20})
+    # plot velocity
+    plt.rcParams.update({'font.size': 10})
     plt.figure(figsize=(30, 30))
     plt.subplot(4, 3, 1)
     plt.plot(t_vec, x_array[:, 3])
@@ -119,7 +117,7 @@ def plot_states_and_inputs(t_vec, x_array, u_array):
     # plot of pitch
     plt.subplot(4, 3, 7)
     plt.plot(t_vec, x_array[:, 10])
-    plt.ylabel('alpha [°]')
+    plt.ylabel('pitch [°]')
     plt.xlabel('time [s]')
 
     # inputs

main.py
@@ -9,15 +9,17 @@ t_end = 10  # duration
 
 # set initial states
 pos_0 = [0.0, 0.0, 0.0]
-vel_0 = [8.0, 0.0, 0.0]
+vel_0 = 8.0
+alpha_0 = 0
+beta_0 = 0
 orient_0 = [0.0, 0.0, 0.0]
 bodyrates_0 = [0.0, 0.0, 0.0]
-x_0 = [pos_0[0], pos_0[1], pos_0[2], vel_0[0], vel_0[1], vel_0[2], orient_0[0], orient_0[1], orient_0[2],
+x_0 = [pos_0[0], pos_0[1], pos_0[2], vel_0, alpha_0, beta_0, orient_0[0], orient_0[1], orient_0[2],
        bodyrates_0[0], bodyrates_0[1], bodyrates_0[2]]
 
 
-u_0 = [0, 0, 10000, 10000]
-
+u_0 = [-5, -5, 10000, 10000]
+# u_elevon_vec = [0, 0, ]
 
 # start simulation
 x = x_0
@@ -51,12 +53,4 @@ while t < t_end:
 
 print("states after simulation: " + str(x))
 
-plot_states_and_inputs(t_vec, x_array, u_array)
-
-# X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
-# C, S = np.cos(X), np.sin(X)
-#
-# plt.plot(X, C)
-# plt.plot(X, S)
-#
-# plt.show()
+# plot_states_and_inputs(t_vec, x_array, u_array)